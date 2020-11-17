c6eeeee071b11628e423cb4cf927be115800ab0b
Angelica <angelica.wilson93@gmail.com>
2020-11-17 15:01:20 -0600

    Phase 2

--- app.js
+++ app.js
@@ -6,11 +6,19 @@ const port = 8081;
 
 app.set('view engine', 'pug');
 
-app.get("/", (req,res) => {
+app.get("/", (req, res) => {
     res.send("Hello from Express!")
 })
 
 
-app.listen(port,() => {
+app.all(/^\/[A-Za-z0-9\-]*$/, (req, res) => {
+    let randomNum = Math.floor(Math.random() * 100);
+    const layoutVars = { randomNum, method: req.method, path: req.path };
+    res.render('layout', layoutVars);
+});
+
+
+
+app.listen(port, () => {
     console.log(`Listening at Port ${port}`)
 })
