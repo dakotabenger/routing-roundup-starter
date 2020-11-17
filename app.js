cee40cb2d30b79b3d0df85679b9bcf4d68ca69d8
Dakota Benger <dakota@bengerlaw.com>
2020-11-17 14:11:22 -0700

    FInished

--- app.js
+++ app.js
@@ -10,8 +10,24 @@ app.get("/", (req, res) => {
     res.send("Hello from Express!")
 })
 
+app.get(/(xyz)$/,(req,res) => {
+    res.send("That's all I wrote.")
+})
 
-app.all(/^\/[A-Za-z0-9\-]*$/, (req, res) => {
+app.get("/capital-letters/:word",(req,res) => {
+    let upperCase = req.params.word.toUpperCase()
+    res.send(upperCase);
+})
+
+app.get("/:name/contact",(req,res) => {
+    res.send("Contact")
+})
+
+app.get("/:name/bio",(req,res) => {
+    res.send("Bio")
+})
+
+app.all(/^\/[A-Za-z0-9\-_]*$/, (req, res) => {
     let randomNum = Math.floor(Math.random() * 100);
     const layoutVars = { randomNum, method: req.method, path: req.path };
     res.render('layout', layoutVars);
